1c39534f38563b034a490168a705e4e041eda55f
MariaClr <n.claraoliveiraramos@gmail.com>
2023-10-19 10:02:59 -0300

    enviando terceiro desafio

--- segundo-desafio.js
+++ segundo-desafio.js
@@ -26,3 +26,35 @@ function partidas(vitorias, derrotas) {
   let nivel = resultado.nivel;
   
   console.log(`O Herói tem saldo de ${saldo} e está no nível de ${nivel}`);
+
+  let um = partidas(3, 5)
+  let s = um.saldoRankeadas
+  console.log( um, s)
+
+console.log(partidas.nivel)
+console.log(partidas(0).saldoRankeadas)
+
+const posicaoInicial = parseInt(gets());
+const totalPassos = parseInt(gets());
+
+//TODO: Calcule a posição final do herói:
+const posicaoFinal = posicaoInicial + posicaoFinal;
+
+// Imprime a posição final
+print("Posicao final do heroi:", posicaoFinal);
+
+
+let escolhaDoTreinador = parseInt(1);
+let pokemonEscolhido = ' '
+
+//TODO: Implemente as condições necessárias para a solução do desafio. Utilize a tabela de exemplos para identificar a escolha do treinador:
+if ( escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur";
+} else if ( escolhaDoTreinador === 2 ) {
+    pokemonEscolhido = "Charmander";
+} else if ( escolhaDoTreinador === 3) {
+    pokemonEscolhido = "Pikachu";
+} else if ( escolhaDoTreinador === 3){
+  pokemonEscolhido = "Pikachu";
+}
+console.log("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.");
